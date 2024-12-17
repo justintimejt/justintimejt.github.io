59f936936368c8d7456f4317716d2f2a2a35b7bd
justintimejt <justinchow3607@gmail.com>
2024-12-17 14:18:53 -0500

    first react component

--- react-intro-kanban/src/App.tsx
+++ react-intro-kanban/src/App.tsx
@@ -1,20 +1,21 @@
 
 import './App.css'
+import TaskCard from './task-card'
+
 
 function App() {
   const title = "Do Market Research"
   const id = "BUS-1"
   const points = 5
+  
   return (
-    <div className="border rounded-lg px-2 m-2 bg-gray-50">
-      <div className="text-base font-semibold py-2">
-        {title}
-      </div>
-      <div className ='flex gap-4 justify-between py-2 text-gray-700'>
-        <div>{id}</div> 
-        <div>{points}</div>
-      </div>
-    </div>
+    <>
+      <TaskCard title = {title} id = {id} points = {points}></TaskCard>
+      <TaskCard title = {title} id = {id} points = {points}></TaskCard>
+      <TaskCard title = {title} id = {id} points = {points}></TaskCard>
+      <TaskCard title = {title} id = {id} points = {points}></TaskCard>
+    </>
+    
   )
   
 }

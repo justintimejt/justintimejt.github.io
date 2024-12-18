c16cab28e4d37e94cebb04f95a70a907be7fba67
justintimejt <justinchow3607@gmail.com>
2024-12-18 12:42:56 -0500

    arrays and statuses

--- react-intro-kanban/src/utils/data-tasks.ts
+++ react-intro-kanban/src/utils/data-tasks.ts
@@ -1,28 +1,37 @@
+
+export type Status = 'todo' | 'in-progress' | 'done'
 export type Task = {
     title: string,
     id: string,
+    status: Status,
     points?: number
   }
 
+  export const statuses: Status[] = ['todo', 'in-progress', 'done']
+  
   export const tasks: Array<Task> = [
     {
       title: 'Do Market',
       id: 'Bus-1',
+      status: 'todo',
       points: 5,
     },
     {
       title: 'Do Market',
       id: 'Bus-1',
+      status: 'todo',
       points: 5,
     },
     {
       title: 'Do Market',
       id: 'Bus-1',
+      status: 'in-progress',
       points: 5,
     },
     {
       title: 'Do Market',
       id: 'Bus-1',
+      status: 'done',
       points: 5,
     }
     ]

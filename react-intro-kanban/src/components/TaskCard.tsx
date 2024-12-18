4c6ccfebe5eb7deb517a1c93be8b25dab11f988a
justintimejt <justinchow3607@gmail.com>
2024-12-18 16:36:15 -0500

    fibbonaci sequence for points

--- react-intro-kanban/src/components/TaskCard.tsx
+++ react-intro-kanban/src/components/TaskCard.tsx
@@ -16,6 +16,18 @@ const TaskCard = ({task, updateTaskPoints}: {
   updateTaskPoints: (task:Task, points: number) => void
 }) => {
   const points = task.points || 0 
+  const updatePoints = (direction: 'up' | 'down') => {
+    
+    const fib = [0, 1, 2, 3, 5, 8, 13]
+    const index = fib.indexOf(points)
+    const nextIndex = direction === 'up' ? index + 1 : index -1
+    const newPoints = fib[nextIndex]
+    if (newPoints) {
+      updateTaskPoints(task, newPoints)
+    }
+
+    updateTaskPoints(task, points)
+  }
     return <div className="border rounded-lg px-2 m-2 bg-gray-50 w-52">
       <div className="text-base font-base py-2">
         {task.title} 
@@ -29,9 +41,9 @@ const TaskCard = ({task, updateTaskPoints}: {
           {task.priority === 'low' && <div>{highPriorityIcon}</div>}
           </div> 
         <div className='flex gap-2 items-center'>
-          <button onClick={() => updateTaskPoints(task, points - 1)}>-</button>
+          <button onClick={() => updatePoints('down')}>-</button>
           <div className='font-bold'>{points}</div>
-          <button onClick={() => updateTaskPoints(task, points + 1)}>+</button>
+          <button onClick={() => updatePoints('up')}>+</button>
         </div>
       </div>
     </div>

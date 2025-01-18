08a1344e4e62f837772671b1e43588dca9269945
justintimejt <justinchow3607@gmail.com>
2025-01-18 09:45:15 -0500

    updated prototype

--- react-intro-kanban/src/components/TaskCard.tsx
+++ react-intro-kanban/src/components/TaskCard.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Task } from "../utils/data-tasks"
 
+//import svg logos
 const lowPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 9l7 7 7-7" />
 </svg>
